db7506d4d789c7265aa8928140131253a6539b26
Matheus Hoffmann <hoffmann@poli.ufrj.br>
2019-05-21 17:50:10 -0300

    added base example

---
+++ Kafka/Producer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+namespace ConsoleApp
+{
+    class Producer
+    {
+        public static async Task Main(string[] args)
+        {
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092" , SaslMechanism = SaslMechanism.ScramSha256, 
+                SecurityProtocol = SecurityProtocol.SaslPlaintext, SaslUsername = "USER_WRITE" , SaslPassword = "PASSWORD"};
+
+            // If serializers are not specified, default serializers from
+            // `Confluent.Kafka.Serializers` will be automatically used where
+            // available. Note: by default strings are encoded as UTF8.
+            using (var p = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    var dr = await p.ProduceAsync("kafka-acl", new Message<Null, string> { Value="test" });
+                    Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+        }
+    }
+}
